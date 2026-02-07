4fcc662461f2cef09594c7f0264921a5ff672930
Rahul Prajapati <rprajapati0799@gmail.com>
2026-02-07 14:43:33 +0530

    fix: correct order route controller import

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { createOrder } = require("../controllers/order.controller");
+
+const createOrder = require("../controllers/order.controller");
 
 router.post("/", createOrder);
 
