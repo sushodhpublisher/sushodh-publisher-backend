dcd7d8e22cd6f8493cf708e3233ce865e76ecc12
Rahul Prajapati <rprajapati0799@gmail.com>
2026-02-09 17:55:08 +0530

    debug contact api hit

--- src/controllers/contact.controller.js
+++ src/controllers/contact.controller.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 
 exports.sendContactMail = async (req, res) => {
+  console.log("CONTACT API HIT", req.body);
   try {
     const { name, email, message } = req.body;
 
