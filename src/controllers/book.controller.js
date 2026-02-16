2d62320d3348816039f117efbbc1a691bda8709f
Rahul Prajapati <rprajapati0799@gmail.com>
2026-02-16 15:50:42 +0530

    Fix: added console log at top of the createbook

--- src/controllers/book.controller.js
+++ src/controllers/book.controller.js
@@ -55,6 +55,10 @@ const uploadToCloudinary = async (fileBuffer) => {
 /* =====================================================
    ADMIN: CREATE BOOK
 ===================================================== */
+
+console.log("All ENV keys:", Object.keys(process.env));
+console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME);
+
 exports.createBook = async (req, res) => {
   try {
     console.log("Incoming Body:", req.body);
