8bfe15da5c859a73fbacf71fdd1c912b326ab6e0
Rahul Prajapati <rprajapati0799@gmail.com>
2026-02-16 20:52:46 +0530

    Fix Cloudinary onstance usage

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,12 +1,10 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME?.trim(),
-  api_key: process.env.CLOUDINARY_API_KEY?.trim(),
-  api_secret: process.env.CLOUDINARY_API_SECRET?.trim(),
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-
-console.log("API KEY CHECK:", process.env.CLOUDINARY_API_KEY);
 
 console.log("Cloudinary Config Loaded:", {
   cloud: process.env.CLOUDINARY_CLOUD_NAME,
